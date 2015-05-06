babc6b4d445851d3e9800629f3ec0de8ab461691
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-06 12:05:29 +0200

    Encapsulate fields

--- Trigger.cs
+++ Trigger.cs
@@ -5,23 +5,24 @@ using UnityEngine.Events;
 
 namespace ActionTrigger {
 
+    /// <summary>
+    /// A multi-purpose script which causes an action to occur on trigger enter.
+    /// </summary>
+    public enum Mode {
+
+        UnityEvent, // Invoke UnityAction
+        Replace, // replace target with source
+        Activate, // Activate the target GameObject
+        Enable, // Enable a component
+        Animate, // Start animation on target
+        Deactivate, // Decativate target GameObject
+        Message // Just broadcast the action on to the target
+
+    }
+
     public sealed class Trigger : MonoBehaviour {
 
-        /// <summary>
-        /// A multi-purpose script which causes an action to occur on trigger enter.
-        /// </summary>
-        public enum Mode {
-
-            UnityEvent, // Invoke UnityAction
-            Replace, // replace target with source
-            Activate, // Activate the target GameObject
-            Enable, // Enable a component
-            Animate, // Start animation on target
-            Deactivate, // Decativate target GameObject
-            Message // Just broadcast the action on to the target
-
-        }
-
+        #region FIELDS
         /// The action to accomplish
         public Mode mode = Mode.Activate;
 
@@ -35,28 +36,72 @@ namespace ActionTrigger {
         public bool repeatTrigger = false;
 
         public UnityEvent action;
+        #endregion
 
+        #region PROPERTIES
+        /// The action to accomplish
+        public Mode Mode {
+            get { return mode; }
+            set { mode = value; }
+        }
+
+        /// The game object to affect. If none, the trigger work on this game object
+        public Object TargetObj {
+            get { return targetObj; }
+            set { targetObj = value; }
+        }
+
+        public GameObject SourceGo {
+            get { return sourceGo; }
+            set { sourceGo = value; }
+        }
+
+        public int TriggerCount {
+            get { return triggerCount; }
+            set { triggerCount = value; }
+        }
+
+        ///
+        public bool RepeatTrigger {
+            get { return repeatTrigger; }
+            set { repeatTrigger = value; }
+        }
+
+        public UnityEvent Action {
+            get { return action; }
+            set { action = value; }
+        }
+
+        #endregion
+
+        #region UNITY MESSAGES
+        private void OnTriggerEnter(Collider other) {
+            DoActivateTrigger();
+        }
+        #endregion
+
+        #region METHODS
         private void DoActivateTrigger() {
-            triggerCount--;
+            TriggerCount--;
 
-            if (triggerCount == 0 || repeatTrigger) {
-                Object currentTarget = targetObj != null ? targetObj : gameObject;
+            if (TriggerCount == 0 || RepeatTrigger) {
+                Object currentTarget = TargetObj != null ? TargetObj : gameObject;
                 Behaviour targetBehaviour = currentTarget as Behaviour;
                 GameObject targetGameObject = currentTarget as GameObject;
                 if (targetBehaviour != null)
                     targetGameObject = targetBehaviour.gameObject;
 
-                switch (mode) {
+                switch (Mode) {
                     case Mode.UnityEvent:
-                        action.Invoke();
+                        Action.Invoke();
                         break;
                     case Mode.Message:
                         targetGameObject.BroadcastMessage("DoActivateTrigger");
                         break;
                     case Mode.Replace:
-                        if (sourceGo != null) {
+                        if (SourceGo != null) {
                             Object.Instantiate(
-                                sourceGo,
+                                SourceGo,
                                 targetGameObject.transform.position,
                                 targetGameObject.transform.rotation);
                             DestroyObject(targetGameObject);
@@ -79,9 +124,7 @@ namespace ActionTrigger {
             }
         }
 
-        private void OnTriggerEnter(Collider other) {
-            DoActivateTrigger();
-        }
+        #endregion
 
     }
 
