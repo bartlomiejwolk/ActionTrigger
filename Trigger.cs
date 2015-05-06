2c278f2f6d65a6aaded78f729460dd19982a6722
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-06 12:06:11 +0200

    Extract enum to file

--- Trigger.cs
+++ Trigger.cs
@@ -5,21 +5,6 @@ using UnityEngine.Events;
 
 namespace ActionTrigger {
 
-    /// <summary>
-    /// A multi-purpose script which causes an action to occur on trigger enter.
-    /// </summary>
-    public enum Mode {
-
-        UnityEvent, // Invoke UnityAction
-        Replace, // replace target with source
-        Activate, // Activate the target GameObject
-        Enable, // Enable a component
-        Animate, // Start animation on target
-        Deactivate, // Decativate target GameObject
-        Message // Just broadcast the action on to the target
-
-    }
-
     public sealed class Trigger : MonoBehaviour {
 
         #region FIELDS
