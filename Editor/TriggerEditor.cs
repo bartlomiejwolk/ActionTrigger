e3900bc306cd34eb8eaa79e5346d409e38756d46
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-06 12:09:38 +0200

    Extract methods

--- Editor/TriggerEditor.cs
+++ Editor/TriggerEditor.cs
@@ -22,6 +22,7 @@ namespace ActionTrigger {
         #endregion
 
 
+        #region UNITY MESSAGES
         private void OnEnable() {
             Script = (Trigger) target;
 
@@ -36,45 +37,68 @@ namespace ActionTrigger {
         public override void OnInspectorGUI() {
             serializedObject.Update();
 
-            EditorGUILayout.PropertyField(
-                mode,
-                new GUIContent(
-                    "Mode",
-                    ""));
+            HandleDrawModeDropdown();
+            HandleDrawSourceGoField();
+            HandleDrawTargetObjField();
+            HandleDrawTriggerCountField();
+            HandleDrawRepeatTriggerToggle();
+            HandleDrawActionField();
 
+            serializedObject.ApplyModifiedProperties();
+        }
 
-            EditorGUILayout.PropertyField(
-                sourceGo,
-                new GUIContent(
-                    "Source Game Object",
-                    ""));
+        #endregion
 
-            EditorGUILayout.PropertyField(
-                targetObj,
-                new GUIContent(
-                    "Target Object",
-                    ""));
-
-            EditorGUILayout.PropertyField(
-                triggerCount,
-                new GUIContent(
-                    "Trigger Count",
-                    ""));
-
-            EditorGUILayout.PropertyField(
-                repeatTrigger,
-                new GUIContent(
-                    "Repeat Trigger",
-                    ""));
-
+        #region INSPECTOR
+        private void HandleDrawActionField() {
             EditorGUILayout.PropertyField(
                 action,
                 new GUIContent(
                     "Action",
                     ""));
-
-            serializedObject.ApplyModifiedProperties();
         }
+
+        private void HandleDrawRepeatTriggerToggle() {
+            EditorGUILayout.PropertyField(
+                repeatTrigger,
+                new GUIContent(
+                    "Repeat Trigger",
+                    ""));
+        }
+
+        private void HandleDrawTriggerCountField() {
+            EditorGUILayout.PropertyField(
+                triggerCount,
+                new GUIContent(
+                    "Trigger Count",
+                    ""));
+        }
+
+        private void HandleDrawTargetObjField() {
+            EditorGUILayout.PropertyField(
+                targetObj,
+                new GUIContent(
+                    "Target Object",
+                    ""));
+        }
+
+        private void HandleDrawSourceGoField() {
+            EditorGUILayout.PropertyField(
+                sourceGo,
+                new GUIContent(
+                    "Source Game Object",
+                    ""));
+        }
+
+        private void HandleDrawModeDropdown() {
+            EditorGUILayout.PropertyField(
+                mode,
+                new GUIContent(
+                    "Mode",
+                    ""));
+        }
+#endregion
+
     }
 
 }
